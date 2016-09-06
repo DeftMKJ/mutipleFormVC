f24285ecf1ad8b9e86e5dda956834499bf8b5faa
Deft_MKJing <mkjing@mkjingdemac-mini.local>
2016-09-06 11:30:35 +0800

    Initial Commit

---
+++ MKJForm/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MKJForm
+//
+//  Created by MKJING on 16/9/6.
+//  Copyright © 2016年 MKJING. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
